a065302aa0be36bc0d6ef3119ad31904a2112730
Davide Berardi <berardi.dav@gmail.com>
2020-11-18 12:51:07 +0100

    gettimeofday prototype fix
    Gettimeofday on Linux is using void *, not struct timezone *.
    This commit set the prototype of the function correctly.

--- src/detime.c
+++ src/detime.c
@@ -21,9 +21,9 @@ time_t time(time_t *res)
 }
 
 #ifdef __unix__
-int gettimeofday(struct timeval *tv, struct timezone *tz)
+int gettimeofday(struct timeval *tv, void *_tz)
 {
-	
+	struct timezone *tz = _tz;
 	char *sec_str = getenv("TV_SEC");
 	char *usec_str = getenv("TV_USEC");
 	tv->tv_sec = sec_str ? atoi(sec_str) : 0;
